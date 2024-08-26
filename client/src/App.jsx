6bc334248598a30961eec929f98f510aea2be3b0
asifmMustafa <asifmustafa.2o5@gmail.com>
2024-08-26 06:51:51 +0600

    input modal component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import Table from "./components/Table";
+import InputModal from "./components/InputModal";
 
 const App = () => {
   return (
     <>
-      <div className="h-screen flex justify-center items-center bg-slate-200">
+      <div className="h-screen flex flex-col justify-center items-center bg-slate-200">
+        <InputModal />
         <Table />
       </div>
     </>
--- client/src/components/InputModal.jsx
+++ client/src/components/InputModal.jsx
@@ -1,7 +1,162 @@
-import React from "react";
+import React, { useState } from "react";
+
+const CloseIcon = () => (
+  <svg
+    className="w-3 h-3"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 14 14"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+    />
+  </svg>
+);
+
+const AddIcon = () => (
+  <svg
+    className="me-1 -ms-1 w-5 h-5"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+);
 
 const InputModal = () => {
-  return <div>InputModal</div>;
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <div>
+      {/* Modal toggle */}
+      <button
+        className="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 mb-10 text-center"
+        type="button"
+        onClick={() => setModalVisible(!modalVisible)}
+      >
+        <AddIcon />
+        Add product
+      </button>
+
+      {/* Main modal */}
+      {modalVisible && (
+        <div className="fixed inset-0 z-50 flex justify-center items-center w-full h-full">
+          {/* Overlay */}
+          <div
+            className="fixed inset-0 bg-gray-800 opacity-75"
+            onClick={() => setModalVisible(false)}
+          ></div>
+
+          {/* Modal content */}
+          <div className="relative p-4 w-full max-w-md">
+            <div className="relative bg-white rounded-lg shadow">
+              {/* Modal header */}
+              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+                <h3 className="text-lg font-semibold text-gray-900">
+                  Create New Product
+                </h3>
+                <button
+                  type="button"
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                  onClick={() => setModalVisible(!modalVisible)}
+                >
+                  <CloseIcon />
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+
+              <form className="p-4 md:p-5">
+                <div className="grid gap-4 mb-4 grid-cols-2">
+                  <div className="col-span-2">
+                    <label
+                      htmlFor="name"
+                      className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                      placeholder="Type product name"
+                      required=""
+                    />
+                  </div>
+                  <div className="col-span-2 sm:col-span-1">
+                    <label
+                      htmlFor="price"
+                      className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                      Price
+                    </label>
+                    <input
+                      type="number"
+                      name="price"
+                      id="price"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                      placeholder="$2999"
+                      required=""
+                    />
+                  </div>
+                  <div className="col-span-2 sm:col-span-1">
+                    <label
+                      htmlFor="category"
+                      className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                      Category
+                    </label>
+                    <select
+                      id="category"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+                    >
+                      <option selected="">Select category</option>
+                      <option value="TV">TV/Monitors</option>
+                      <option value="PC">PC</option>
+                      <option value="GA">Gaming/Console</option>
+                      <option value="PH">Phones</option>
+                    </select>
+                  </div>
+                  <div className="col-span-2">
+                    <label
+                      htmlFor="description"
+                      className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                      Product Description
+                    </label>
+                    <textarea
+                      id="description"
+                      rows="4"
+                      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                      placeholder="Write product description here"
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="w-full grid place-content-center">
+                  <button
+                    type="submit"
+                    className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  >
+                    Add
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default InputModal;
